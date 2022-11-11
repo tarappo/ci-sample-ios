bcab8da820f5196ed63ccac4952f39003835d8ba
tarappo <tarappo@gmail.com>
2022-11-12 08:03:24 +0900

    add complexity code

--- ci-sample/Sample.swift
+++ ci-sample/Sample.swift
@@ -12,5 +12,33 @@ class Sample {
     func add(_ n1: Int, _ n2: Int) -> Int {
         return n1 + n2
     }
-    
+
+    // Complexity code
+    func sampleA(number: Int) -> Bool {
+        if(number > 2) {
+            if(number > 3) {
+                if(number > 4) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+        return false
+    }
+
+    func sampleB(number: Int) -> Bool {
+        switch(number) {
+        case 1:
+            return false
+        case 2:
+            return true
+        case 3:
+            return false
+        case 4:
+            return true
+        default:
+            return false
+        }
+    }
 }
